e69a8e0413a727a4d86d0e5811382951f881ca98
Diogo Almeida <diogoalmeida.vlc@gmail.com>
2023-04-19 17:10:57 +0100

    ups

--- src/main/pt/up/fe/comp2023/jasmin/JasminBackEnd.java
+++ src/main/pt/up/fe/comp2023/jasmin/JasminBackEnd.java
@@ -1,4 +1,4 @@
-package pt.up.fe.comp2023;
+package pt.up.fe.comp2023.jasmin;
 import org.specs.comp.ollir.ClassUnit;
 import org.specs.comp.ollir.OllirErrorException;
 import pt.up.fe.comp.jmm.jasmin.JasminBackend;
@@ -6,6 +6,7 @@ import pt.up.fe.comp.jmm.jasmin.JasminResult;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.comp2023.jasmin.JasminGenerator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/main/pt/up/fe/comp2023/jasmin/JasminGenerator.java
+++ src/main/pt/up/fe/comp2023/jasmin/JasminGenerator.java
@@ -1,4 +1,4 @@
-package pt.up.fe.comp2023;
+package pt.up.fe.comp2023.jasmin;
 
 import org.specs.comp.ollir.*;
 
