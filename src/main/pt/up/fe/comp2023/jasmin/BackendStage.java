1fecb48b156c50e3e0df72dfe0d83c0b1c95964b
Diogo Almeida <diogoalmeida.vlc@gmail.com>
2023-04-21 11:28:06 +0100

    revert changes of merge

--- src/main/pt/up/fe/comp2023/jasmin/BackendStage.java
+++ src/main/pt/up/fe/comp2023/jasmin/BackendStage.java
@@ -1,26 +1,35 @@
 package pt.up.fe.comp2023.jasmin;
 
 import org.specs.comp.ollir.ClassUnit;
+import org.specs.comp.ollir.OllirErrorException;
 import pt.up.fe.comp.jmm.jasmin.JasminBackend;
 import pt.up.fe.comp.jmm.jasmin.JasminResult;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp.jmm.report.Report;
-import pt.up.fe.comp2023.jasmin.JasminGenerator;
+import pt.up.fe.comp.jmm.report.Stage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BackendStage implements JasminBackend {
     @Override
     public JasminResult toJasmin(OllirResult ollirResult) {
         ClassUnit ollirClass = ollirResult.getOllirClass();
-        System.out.println("----------------------------");
-        String jasminCode = new JasminGenerator(ollirClass).dealWithClass();
-        System.out.println(jasminCode);
-        System.out.println("--------------------------");
+        try{
+            ollirClass.checkMethodLabels();
+            ollirClass.buildCFGs();
+            ollirClass.outputCFGs();
+            ollirClass.buildVarTables();
+            ollirClass.show();
 
-        List<Report> reports = new ArrayList<>();
+            String JasminCode = new JasminGenerator(ollirClass).dealWithClass();
 
-        return new JasminResult(ollirResult, jasminCode, reports);
+            List<Report> reports = new ArrayList<>();
+            return new JasminResult(ollirResult, JasminCode, reports);
+        } catch (OllirErrorException f){
+            return new JasminResult(ollirClass.getClassName(), null, Arrays.asList(Report.newError(Stage.GENERATION, -1, -1,
+                    "Expection during Jasmin Generator", f)));
+        }
     }
 }
